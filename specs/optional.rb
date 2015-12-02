1e3a50fbb87c156c3938da957bc5dc152e23c6ee
Wolfram Kriesing <wolfram+github@kriesing.de>
2015-12-02 13:16:57 +0100

    The initial thing we want to beautify.

--- specs/optional.rb
+++ specs/optional.rb
@@ -6,4 +6,41 @@ class OptionalTests < MiniTest::Unit::TestCase
     assert true
   end
   
+  def test_working_chain
+    user_name = 'Bob'
+    user = User.new(user_name)
+    member = Member.new(user)
+    crew = Crew.new(member)
+    
+    assert_equal crew.owner.user.name, user_name
+  end
+  
+end
+
+  
+class Crew
+  def initialize(owner)
+    @owner = owner
+  end
+  def owner
+    @owner
+  end
+end
+
+class Member
+  def initialize(user)
+    @user = user
+  end
+  def user
+    @user
+  end
+end
+
+class User
+  def initialize(name)
+    @name = name
+  end
+  def name
+    @name
+  end
 end
